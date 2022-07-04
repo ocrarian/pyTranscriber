c27fbf12954079777f84161b34d59e713219f3ae
yshalsager <ysh-alsager@hotmail.com>
2022-07-04 20:12:41 +0200

    refactor: Use autosub directly [WIP]
    * Only works with my fork using witai now

--- pytranscriber/control/ctr_autosub.py
+++ pytranscriber/control/ctr_autosub.py
@@ -1,21 +1,35 @@
-from autosub import FLACConverter
-from autosub import SpeechRecognizer
-from autosub import extract_audio
-from autosub import find_speech_regions
-from autosub import DEFAULT_CONCURRENCY
-from autosub import DEFAULT_SUBTITLE_FORMAT
-from autosub import GOOGLE_SPEECH_API_KEY
-from autosub.formatters import FORMATTERS
-
 import multiprocessing
-import time
-import os
+from os import name, uname
+from pathlib import Path
+from sys import path
+from time import sleep
 
+# Append cli to path
+
+path.insert(0, str(Path(__file__).parent.parent.parent / "autosub_cli"))
+
+from autosub_cli.autosub.api_wit_ai import WITAiAPI
+from autosub_cli.autosub.auditok_utils import auditok_gen_speech_regions
+from autosub_cli.autosub.cmdline_utils import convert_wav
+from autosub_cli.autosub.constants import (
+    DEFAULT_AUDIO_CVT_CMD,
+    DEFAULT_AUDIO_SPLT_CMD,
+    DEFAULT_CONCURRENCY,
+    DEFAULT_SUBTITLES_FORMAT,
+    WIT_AI_API_URL,
+)
+from autosub_cli.autosub.core import list_to_sub_str
+from autosub_cli.autosub.ffmpeg_utils import SplitIntoAudioPiece
 from pytranscriber.util.util import MyUtil
 
+api_url = f"https://{WIT_AI_API_URL}"
+api_key = ""
 
-class Ctr_Autosub():
+# TODO add settings
+# TODO add other languages support
 
+
+class Ctr_Autosub:
     cancel = False
 
     @staticmethod
@@ -25,7 +39,6 @@ class Ctr_Autosub():
     @staticmethod
     def is_operation_canceled():
         return Ctr_Autosub.cancel
-
 
     @staticmethod
     def output_progress(listener_progress, str_task, progress_percent):
@@ -38,7 +51,7 @@ class Ctr_Autosub():
         Ctr_Autosub.cancel = True
 
         while Ctr_Autosub.step == 0:
-            time.sleep(0.1)
+            sleep(0.1)
 
         # the first step involves ffmpeg and cannot be stopped safely
         if Ctr_Autosub.step == 1:
@@ -52,89 +65,102 @@ class Ctr_Autosub():
             Ctr_Autosub.pool.join()
 
     @staticmethod
-    def generate_subtitles(# pylint: disable=too-many-locals,too-many-arguments
-            source_path,
-            src_language,
-            listener_progress,
-            output=None,
-            concurrency=DEFAULT_CONCURRENCY,
-            subtitle_file_format=DEFAULT_SUBTITLE_FORMAT
-        ):
+    def generate_subtitles(  # pylint: disable=too-many-locals,too-many-arguments
+        source_path,
+        src_language,
+        listener_progress,
+        output=None,
+        concurrency=DEFAULT_CONCURRENCY,
+        # subtitle_file_format=DEFAULT_SUBTITLES_FORMAT,
+    ):
 
         # windows not support forkserver... only spawn
-        if os.name != "nt" and "Darwin" in os.uname():
+        if name != "nt" and "Darwin" in uname():
             # necessary for running on MacOS
             # method can be set only once, otherwise crash
-            #from python 3.8 above the default for macos is spawn and not fork
-            if 'spawn' != multiprocessing.get_start_method(allow_none=True):
-                multiprocessing.set_start_method('spawn')
+            # from python 3.8 above the default for macos is spawn and not fork
+            if "spawn" != multiprocessing.get_start_method(allow_none=True):
+                multiprocessing.set_start_method("spawn")
         Ctr_Autosub.cancel = False
         Ctr_Autosub.step = 0
-        """
-        Given an input audio/video file, generate subtitles in the specified language and format.
-        """
-        audio_filename, audio_rate = extract_audio(source_path)
-
-        regions = find_speech_regions(audio_filename)
-
-        converter = FLACConverter(source_path=audio_filename)
-        recognizer = SpeechRecognizer(language=src_language, rate=audio_rate,
-                                      api_key=GOOGLE_SPEECH_API_KEY)
+        # Given an input audio/video file, generate subtitles in the specified language and format.
+        Ctr_Autosub.output_progress(
+            listener_progress, "Step 1 of 4: Converting input file to wav ", 1
+        )
+        audio_wav = convert_wav(
+            input_=source_path,
+            conversion_cmd=DEFAULT_AUDIO_CVT_CMD,
+        )
+        regions = auditok_gen_speech_regions(
+            audio_wav=audio_wav, mode=0
+        )  # mode=0 => autosub's args.auditok_mode
+        if not regions:
+            return
+        converter = SplitIntoAudioPiece(
+            source_path=source_path,
+            output=output,
+            is_keep=False,
+            cmd=DEFAULT_AUDIO_SPLT_CMD.replace("-vn", "-vn -c:a pcm_s16le -f s16le")
+            .replace("[channel]", "1")
+            .replace("[sample_rate]", "8000"),
+            suffix=".pcm",
+        )  # TODO allow more values
+        recognizer = WITAiAPI(api_url=api_url, api_key=api_key)
         transcripts = []
-        if regions:
-            try:
-                if Ctr_Autosub.cancel:
-                    return -1
-
-                str_task_1 = "Step 1 of 2: Converting speech regions to FLAC files "
-                len_regions = len(regions)
-                extracted_regions = []
-                Ctr_Autosub.pool = multiprocessing.Pool(concurrency)
-                for i, extracted_region in enumerate(Ctr_Autosub.pool.imap(converter, regions)):
-                    Ctr_Autosub.step = 1
-                    extracted_regions.append(extracted_region)
-                    progress_percent = MyUtil.percentage(i, len_regions)
-                    Ctr_Autosub.output_progress(listener_progress, str_task_1, progress_percent)
-                if Ctr_Autosub.cancel:
-                    return -1
-                else:
-                    Ctr_Autosub.pool.close()
-                    Ctr_Autosub.pool.join()
-
-                str_task_2 = "Step 2 of 2: Performing speech recognition "
-                Ctr_Autosub.pool = multiprocessing.Pool(concurrency)
-                for i, transcript in enumerate(Ctr_Autosub.pool.imap(recognizer, extracted_regions)):
-                    Ctr_Autosub.step = 2
-                    transcripts.append(transcript)
-                    progress_percent = MyUtil.percentage(i, len_regions)
-                    Ctr_Autosub.output_progress(listener_progress, str_task_2, progress_percent)
-
-                if Ctr_Autosub.cancel:
-                    return -1
-                else:
-                    Ctr_Autosub.pool.close()
-                    Ctr_Autosub.pool.join()
-
-            except KeyboardInterrupt:
-                Ctr_Autosub.pbar.finish()
-                Ctr_Autosub.pool.terminate()
+        try:
+            if Ctr_Autosub.cancel:
+                return -1
+            str_task_1 = "Step 2 of 4: Recognizing speech regions "
+            len_regions = len(regions)
+            audio_fragments = []
+            Ctr_Autosub.pool = multiprocessing.Pool(concurrency)
+            for i, audio_fragment in enumerate(
+                Ctr_Autosub.pool.imap(converter, regions)
+            ):
+                Ctr_Autosub.step = 1
+                if audio_fragment:
+                    audio_fragments.append(audio_fragment)
+                progress_percent = MyUtil.percentage(i, len_regions)
+                Ctr_Autosub.output_progress(
+                    listener_progress, str_task_1, progress_percent
+                )
+            if not audio_fragments:
+                return
+            if Ctr_Autosub.cancel:
+                return -1
+            else:
+                Ctr_Autosub.pool.close()
                 Ctr_Autosub.pool.join()
-                raise
 
-        timed_subtitles = [(r, t) for r, t in zip(regions, transcripts) if t]
-        formatter = FORMATTERS.get(subtitle_file_format)
-        formatted_subtitles = formatter(timed_subtitles)
+            str_task_2 = "Step 3 of 4: Performing speech recognition "
+            Ctr_Autosub.pool = multiprocessing.Pool(concurrency)
+            for i, transcript in enumerate(
+                Ctr_Autosub.pool.imap(recognizer, audio_fragments)
+            ):
+                Ctr_Autosub.step = 2
+                transcripts.append(transcript)
+                progress_percent = MyUtil.percentage(i, len_regions)
+                Ctr_Autosub.output_progress(
+                    listener_progress, str_task_2, progress_percent
+                )
 
-        dest = output
+            if Ctr_Autosub.cancel:
+                return -1
+            else:
+                Ctr_Autosub.pool.close()
+                Ctr_Autosub.pool.join()
 
-        if not dest:
-            base = os.path.splitext(source_path)[0]
-            dest = "{base}.{format}".format(base=base, format=subtitle_file_format)
+        except KeyboardInterrupt:
+            Ctr_Autosub.pbar.finish()
+            Ctr_Autosub.pool.terminate()
+            Ctr_Autosub.pool.join()
+            raise
 
-        with open(dest, 'wb') as output_file:
-            output_file.write(formatted_subtitles.encode("utf-8"))
-
-        os.remove(audio_filename)
+        timed_subtitles = [
+            (region, text) for region, text in zip(regions, transcripts) if text
+        ]
+        subtitle = list_to_sub_str(timed_subtitles)
+        output.write_bytes(subtitle.encode("utf-8"))
 
         if Ctr_Autosub.cancel:
             return -1
@@ -142,4 +168,4 @@ class Ctr_Autosub():
             Ctr_Autosub.pool.close()
             Ctr_Autosub.pool.join()
 
-        return dest
+        return output
