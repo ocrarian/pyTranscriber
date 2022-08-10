35db1f8b1aacb5bd24d67494fefb3fb6fb549e6a
yshalsager <ysh-alsager@hotmail.com>
2022-08-10 16:48:13 +0200

    style: reformat the code with black

--- pytranscriber/control/thread_exec_autosub.py
+++ pytranscriber/control/thread_exec_autosub.py
@@ -1,4 +1,4 @@
-'''
+"""
    (C) 2019 Raryel C. Souza
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -10,7 +10,7 @@
     GNU General Public License for more details.
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
-'''
+"""
 
 from PySide6 import QtCore
 from pathlib import Path
@@ -33,20 +33,22 @@ class Thread_Exec_Autosub(QtCore.QThread):
         self.running = True
         QtCore.QThread.__init__(self)
 
-    def __updateProgressFileYofN(self, currentIndex, countFiles ):
-        self.updating_file_progress.emit("File " + str(currentIndex+1) + " of " +str(countFiles))
+    def __updateProgressFileYofN(self, currentIndex, countFiles):
+        self.updating_file_progress.emit(
+            "File " + str(currentIndex + 1) + " of " + str(countFiles)
+        )
 
     def listenerProgress(self, string, percent):
         self.updateing_progress.emit(string, percent)
 
     def __generatePathOutputFile(self, sourceFile):
-        #extract the filename without extension from the path
+        # extract the filename without extension from the path
         base = os.path.basename(sourceFile)
-        #[0] is filename, [1] is file extension
+        # [0] is filename, [1] is file extension
         fileName = os.path.splitext(base)[0]
 
-        #the output file has same name as input file, located on output Folder
-        #with extension .srt
+        # the output file has same name as input file, located on output Folder
+        # with extension .srt
         pathOutputFolder = Path(self.objParamAutosub.outputFolder)
         outputFileSRT = pathOutputFolder / (fileName + ".srt")
         outputFileTXT = pathOutputFolder / (fileName + ".txt")
@@ -58,25 +60,30 @@ class Thread_Exec_Autosub(QtCore.QThread):
         outputFileSRT = outputFiles[0]
         outputFileTXT = outputFiles[1]
 
-        #run autosub
-        fOutput = Ctr_Autosub.generate_subtitles(source_path = sourceFile,
-                                    output = outputFileSRT,
-                                    src_language = langCode,
-                                    listener_progress = self.listenerProgress)
-        #if nothing was returned
+        # run autosub
+        fOutput = Ctr_Autosub.generate_subtitles(
+            source_path=sourceFile,
+            output=outputFileSRT,
+            src_language=langCode,
+            listener_progress=self.listenerProgress,
+        )
+        # if nothing was returned
         if not fOutput:
-            self.sending_message.emit("error", "Error! Unable to generate subtitles for file " + sourceFile + ".")
+            self.sending_message.emit(
+                "error",
+                "Error! Unable to generate subtitles for file " + sourceFile + ".",
+            )
         elif fOutput != -1:
-            #if the operation was not canceled
+            # if the operation was not canceled
 
-            #updated the progress message
+            # updated the progress message
             self.listenerProgress("Finished", 100)
 
-            #parses the .srt subtitle file and export text to .txt file
+            # parses the .srt subtitle file and export text to .txt file
             SRTParser.extractTextFromSRT(str(outputFileSRT))
 
             if self.objParamAutosub.boolOpenOutputFilesAuto:
-                #open both SRT and TXT output files
+                # open both SRT and TXT output files
                 MyUtil.open_file(outputFileTXT)
                 MyUtil.open_file(outputFileSRT)
 
@@ -85,30 +92,31 @@ class Thread_Exec_Autosub(QtCore.QThread):
 
         langCode = self.objParamAutosub.langCode
 
-        #if output directory does not exist, creates it
+        # if output directory does not exist, creates it
         pathOutputFolder = Path(self.objParamAutosub.outputFolder)
 
         if not os.path.exists(pathOutputFolder):
             os.mkdir(pathOutputFolder)
-        #if there the output file is not a directory
+        # if there the output file is not a directory
         if not os.path.isdir(pathOutputFolder):
-            #force the user to select a different output directory
-            self.sending_message.emit("error", "Error! Invalid output folder. Please choose another one.")
+            # force the user to select a different output directory
+            self.sending_message.emit(
+                "error", "Error! Invalid output folder. Please choose another one."
+            )
         else:
-            #go ahead with autosub process
+            # go ahead with autosub process
             nFiles = len(self.objParamAutosub.listFiles)
             for i in range(nFiles):
-                #does not continue the loop if user clicked cancel button
+                # does not continue the loop if user clicked cancel button
                 if not Ctr_Autosub.is_operation_canceled():
                     self.__updateProgressFileYofN(i, nFiles)
                     self.__runAutosubForMedia(i, langCode)
 
-            #if operation is canceled does not clear the file list
+            # if operation is canceled does not clear the file list
             if Ctr_Autosub.is_operation_canceled():
                 self.reseting_gui_after_cancel.emit()
             else:
                 self.reseting_gui_after_success.emit()
-
 
     def run(self):
         Ctr_Autosub.init()
@@ -116,4 +124,4 @@ class Thread_Exec_Autosub(QtCore.QThread):
         self.running = False
 
     def cancel(self):
-       Ctr_Autosub.cancel_operation()
+        Ctr_Autosub.cancel_operation()
