38ffeb25b1b410937291431e561c310a4fa11d95
zer0-x <zer0-x.7ty50@aleeas.com>
2022-11-25 15:21:22 +0300

    Create __about__.py file

---
+++ pytranscriber/__about__.py
@@ -0,0 +1,10 @@
+"""Information about the applicatin."""
+
+# TODO: Add an application id.
+APP_ID = ""
+
+PROJECT_HOME_PAGE_URL = "https://github.com/ocrarian/pyTranscriber"
+BUG_REPORT_URL = "https://github.com/ocrarian/pyTranscriber/issues/new/choose"
+
+# TODO: Add an application version.
+APP_VERSION = ""
